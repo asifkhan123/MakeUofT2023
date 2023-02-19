46db2619d4f6b952fad2abdc413f7c15dbcd8942
Asif Khan <35867614+asifkhan123@users.noreply.github.com>
2023-02-18 19:35:48 -0500

    now prints out notes (inaccurate)

--- test.ino
+++ test.ino
@@ -8,12 +8,13 @@
 #define BUTTON_PIN7 51
 #define BUTTON_PIN1 53
 
-LiquidCrystal lcd(11, 12, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  lcd.begin(16,2);
+  lcd.begin(16, 2);
+  lcd.setCursor(2, 1);
   pinMode(BUTTON_PIN0, INPUT_PULLUP);
   pinMode(BUTTON_PIN1, INPUT_PULLUP);
   pinMode(BUTTON_PIN2, INPUT_PULLUP);
@@ -23,11 +24,10 @@ void setup() {
   pinMode(BUTTON_PIN6, INPUT_PULLUP);
   pinMode(BUTTON_PIN7, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
+  delay(4000);
 }
 
-void loop()
-{
-  lcd.print("balls");
+void loop() {
   checkPush(BUTTON_PIN7);
   checkPush(BUTTON_PIN6);
   checkPush(BUTTON_PIN5);
@@ -38,11 +38,44 @@ void loop()
   checkPush(BUTTON_PIN0);
 }
 
-void checkPush(int pinNumber){
+void checkPush(int pinNumber) {
   int buttonPushed = digitalRead(pinNumber);
-  if (buttonPushed == HIGH){
-    lcd.print(pinNumber);
+  if (buttonPushed == HIGH) {
+    lcd.setCursor(7, 0);
+    lcd.print("hi");
+    printNote(pinNumber);
     delay(100);
+    lcd.clear();
+  } else {
   }
-  else{}
+}
+
+void printNote(int pinNumber) {
+  lcd.setCursor(7, 1);
+  switch (pinNumber){
+  case 39:
+    lcd.print("C");
+  break;
+  case 41:
+    lcd.print("D");
+    break;
+  case 43:
+    lcd.print("E");
+    break;
+  case 45:
+    lcd.print("F");
+    break;
+  case 47:
+    lcd.print("G");
+    break;
+  case 49:
+    lcd.print("A");
+    break;
+  case 51:
+    lcd.print("B");
+    break;
+  case 53:
+    lcd.print("C");
+    break;
+}
 }
